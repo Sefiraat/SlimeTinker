85decdb7f48521a78794ea30dc1351e89cab09f1
Sefiraat <miakis@me.com>
2021-06-10 18:40:38 +0100

    Initial Commit

---
+++ src/main/java/io/github/sefiraat/slimetinker/SlimeTinker.java
@@ -0,0 +1,27 @@
+package io.github.sefiraat.slimetinker;
+
+import io.github.mooy1.infinitylib.AbstractAddon;
+
+public class SlimeTinker extends AbstractAddon {
+
+    @Override
+    protected void enable() {
+
+    }
+
+    @Override
+    protected void disable() {
+
+    }
+
+    @Override
+    protected String getGithubPath() {
+        return "Sefiraat/SlimeTinker/master";
+    }
+
+    @Override
+    public String getAutoUpdatePath() {
+        return null;
+    }
+
+}

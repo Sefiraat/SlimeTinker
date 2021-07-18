76691eeb4c8a0c63844353357c33bf4bc7413cb0
Sefiraat <miakis@me.com>
2021-07-18 14:18:39 +0100

    Fixes coal tools, block breaking events for SF blocks and failing smelteries on restart

--- src/main/java/io/github/sefiraat/slimetinker/runnables/FirstTick.java
+++ src/main/java/io/github/sefiraat/slimetinker/runnables/FirstTick.java
@@ -1,9 +1,6 @@
 package io.github.sefiraat.slimetinker.runnables;
 
 import io.github.sefiraat.slimetinker.SlimeTinker;
-import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
-import me.mrCookieSlime.Slimefun.Lists.RecipeType;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class FirstTick extends BukkitRunnable {

47115d63466348209ea31df0dd503dd5e771b413
Sefiraat <miakis@me.com>
2021-12-18 15:39:25 +0000

    Slimefun Dev 987/ MC 1.18 + update

--- src/main/java/io/github/sefiraat/slimetinker/managers/SupportedPluginsManager.java
+++ src/main/java/io/github/sefiraat/slimetinker/managers/SupportedPluginsManager.java
@@ -3,20 +3,29 @@ package io.github.sefiraat.slimetinker.managers;
 import io.github.sefiraat.slimetinker.SlimeTinker;
 import io.github.sefiraat.slimetinker.utils.ThemeUtils;
 import lombok.experimental.UtilityClass;
+import org.bukkit.plugin.PluginManager;
 
 @SuppressWarnings("SpellCheckingInspection")
 @UtilityClass
 public final class SupportedPluginsManager {
 
-    public static final boolean INFINITY_EXPANSION = SlimeTinker.inst().getServer().getPluginManager().getPlugin("InfinityExpansion") != null;
-    public static final boolean SLIMEFUN_WARFARE = SlimeTinker.inst().getServer().getPluginManager().getPlugin("SlimefunWarfare") != null;
-    public static final boolean DYNATECH = SlimeTinker.inst().getServer().getPluginManager().getPlugin("DynaTech") != null;
-    public static final boolean LITEXPANSION = SlimeTinker.inst().getServer().getPluginManager().getPlugin("LiteXpansion") != null;
+    public static final boolean INFINITY_EXPANSION;
+    public static final boolean SLIMEFUN_WARFARE;
+    public static final boolean DYNATECH;
+    public static final boolean LITEXPANSION;
 
     public static final String CORE_NOTE = ThemeUtils.MAIN + "Core SlimeTinker";
     public static final String INFINITY_EXPANSION_NOTE = ThemeUtils.ADD_INFINITY + "Infinity Expansion";
     public static final String SLIMEFUN_WARFARE_NOTE = ThemeUtils.ADD_SLIMEFUN_WARFARE + "Slimefun Warfare";
     public static final String DYNATECH_NOTE = ThemeUtils.ADD_DYNATECH + "DynaTech";
     public static final String LITEXPANSION_NOTE = ThemeUtils.ADD_LITEXPANSION + "LiteXpansion";
+
+    static {
+        PluginManager pluginManager = SlimeTinker.inst().getServer().getPluginManager();
+        INFINITY_EXPANSION = pluginManager.isPluginEnabled("InfinityExpansion");
+        SLIMEFUN_WARFARE = pluginManager.isPluginEnabled("SlimefunWarfare");
+        DYNATECH = pluginManager.isPluginEnabled("DynaTech");
+        LITEXPANSION = pluginManager.isPluginEnabled("LiteXpansion");
+    }
 
 }
--- src/main/java/io/github/sefiraat/slimetinker/utils/WorldUtils.java
+++ src/main/java/io/github/sefiraat/slimetinker/utils/WorldUtils.java
@@ -1,12 +1,10 @@
 package io.github.sefiraat.slimetinker.utils;
 
-import io.github.sefiraat.slimetinker.utils.enums.Temperature;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang.Validate;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -34,110 +32,6 @@ public class WorldUtils {
 
     public static void dropItem(World w, Location l, ItemStack i) {
         w.dropItemNaturally(l, i);
-    }
-
-
-    public static boolean isBiomeHot(Biome biome) {
-        return getBiomeTemperature(biome) == Temperature.HOT;
-    }
-
-    public static boolean isBiomeCold(Biome biome) {
-        return getBiomeTemperature(biome) == Temperature.COLD;
-    }
-
-    public static boolean isBiomeNormal(Biome biome) {
-        return getBiomeTemperature(biome) == Temperature.NORMAL;
-    }
-
-    public static Temperature getBiomeTemperature(Biome biome) {
-        switch (biome) {
-            case DESERT:
-            case SWAMP:
-            case NETHER_WASTES:
-            case BEACH:
-            case DESERT_HILLS:
-            case JUNGLE:
-            case JUNGLE_HILLS:
-            case JUNGLE_EDGE:
-            case SAVANNA:
-            case SAVANNA_PLATEAU:
-            case WOODED_BADLANDS_PLATEAU:
-            case BADLANDS:
-            case BADLANDS_PLATEAU:
-            case WARM_OCEAN:
-            case LUKEWARM_OCEAN:
-            case DEEP_WARM_OCEAN:
-            case DEEP_LUKEWARM_OCEAN:
-            case DESERT_LAKES:
-            case SWAMP_HILLS:
-            case MODIFIED_JUNGLE:
-            case MODIFIED_JUNGLE_EDGE:
-            case SHATTERED_SAVANNA:
-            case SHATTERED_SAVANNA_PLATEAU:
-            case ERODED_BADLANDS:
-            case MODIFIED_WOODED_BADLANDS_PLATEAU:
-            case MODIFIED_BADLANDS_PLATEAU:
-            case BAMBOO_JUNGLE:
-            case BAMBOO_JUNGLE_HILLS:
-            case SOUL_SAND_VALLEY:
-            case CRIMSON_FOREST:
-            case WARPED_FOREST:
-            case BASALT_DELTAS:
-                return Temperature.HOT;
-            case OCEAN:
-            case MOUNTAINS:
-            case THE_END:
-            case FROZEN_OCEAN:
-            case FROZEN_RIVER:
-            case SNOWY_TUNDRA:
-            case SNOWY_MOUNTAINS:
-            case DEEP_OCEAN:
-            case SNOWY_BEACH:
-            case SNOWY_TAIGA:
-            case SNOWY_TAIGA_HILLS:
-            case SMALL_END_ISLANDS:
-            case END_MIDLANDS:
-            case END_HIGHLANDS:
-            case END_BARRENS:
-            case COLD_OCEAN:
-            case DEEP_COLD_OCEAN:
-            case DEEP_FROZEN_OCEAN:
-            case THE_VOID:
-            case ICE_SPIKES:
-            case SNOWY_TAIGA_MOUNTAINS:
-                return Temperature.COLD;
-            case PLAINS:
-            case FOREST:
-            case TAIGA:
-            case RIVER:
-            case MUSHROOM_FIELDS:
-            case MUSHROOM_FIELD_SHORE:
-            case WOODED_HILLS:
-            case TAIGA_HILLS:
-            case MOUNTAIN_EDGE:
-            case STONE_SHORE:
-            case BIRCH_FOREST:
-            case BIRCH_FOREST_HILLS:
-            case DARK_FOREST:
-            case GIANT_TREE_TAIGA:
-            case GIANT_TREE_TAIGA_HILLS:
-            case WOODED_MOUNTAINS:
-            case SUNFLOWER_PLAINS:
-            case GRAVELLY_MOUNTAINS:
-            case FLOWER_FOREST:
-            case TAIGA_MOUNTAINS:
-            case TALL_BIRCH_FOREST:
-            case TALL_BIRCH_HILLS:
-            case DARK_FOREST_HILLS:
-            case GIANT_SPRUCE_TAIGA:
-            case GIANT_SPRUCE_TAIGA_HILLS:
-            case MODIFIED_GRAVELLY_MOUNTAINS:
-            case DRIPSTONE_CAVES:
-            case LUSH_CAVES:
-            case CUSTOM:
-            default:
-                return Temperature.NORMAL;
-        }
     }
 
     public static Location getMid(World world, double x1, double y1, double z1, double x2, double y2, double z2) {

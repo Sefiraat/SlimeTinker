2bf3ab8546bcf47b8badf4bf577c5c759eca3733
Sefiraat <miakis@me.com>
2022-06-12 22:09:21 +0100

    Remove commons

--- src/main/java/io/github/sefiraat/slimetinker/utils/ThemeUtils.java
+++ src/main/java/io/github/sefiraat/slimetinker/utils/ThemeUtils.java
@@ -3,8 +3,6 @@ package io.github.sefiraat.slimetinker.utils;
 import io.github.sefiraat.slimetinker.items.BaseItem;
 import io.github.sefiraat.slimetinker.utils.enums.ThemeItemType;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
-import io.github.thebusybiscuit.slimefun4.libraries.commons.lang.StringUtils;
-import io.github.thebusybiscuit.slimefun4.libraries.commons.lang.WordUtils;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -246,13 +244,39 @@ public final class ThemeUtils {
     }
 
     @Nonnull
-    public static String toTitleCase(String string) {
-        final char[] delimiters = {' ', '_'};
-        return WordUtils.capitalizeFully(string, delimiters).replace("_", " ");
+    public static String toTitleCase(@Nonnull String string) {
+        return toTitleCase(string, true, " _'-/");
+    }
+
+    @Nonnull
+    public static String toTitleCase(@Nonnull String string, boolean delimiterToSpace) {
+        return toTitleCase(string, delimiterToSpace, " _'-/");
+    }
+
+    @Nonnull
+    public static String toTitleCase(@Nonnull String string, boolean delimiterToSpace, @Nonnull String delimiters) {
+        final StringBuilder builder = new StringBuilder();
+        boolean capNext = true;
+
+        for (char character : string.toCharArray()) {
+            character = (capNext) ? Character.toUpperCase(character) : Character.toLowerCase(character);
+            builder.append(character);
+            capNext = (delimiters.indexOf(character) >= 0);
+        }
+
+        String built = builder.toString();
+
+        if (delimiterToSpace) {
+            final char space = ' ';
+            for (char c : delimiters.toCharArray()) {
+                built = built.replace(c, space);
+            }
+        }
+        return built;
     }
 
     @Nonnull
     public static String getLine() {
-        return PASSIVE + StringUtils.repeat("-", 25);
+        return PASSIVE + "-".repeat(25);
     }
 }
